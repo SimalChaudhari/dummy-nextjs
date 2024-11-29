945ce448eebc86f3075b64fbfe6b6eed056617dc
zangula-dev <simalc@zangula.com>
2024-11-29 15:48:01 +0530

    Add

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
-
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [UserModule] // Import UserModule here
+  imports: [UserModule],  // Import UserModule here
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
